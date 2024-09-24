0b48cc4862174a8fd6e51dd6e7013f7223273c5d
jbrough <jamie1612@gmail.com>
2024-09-24 04:27:06 +0100

    rm moved modules

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,5 @@ use bytes::Bytes;
 #[macro_use]
 extern crate mpeg2ts_reader;
 
-pub mod ip;
-
-// SRT listener
-// pub mod listener;
-// mpeg ts PES stream demuxing
-// pub mod demuxer;
-// m3u8 playlist ringbuffers
-//pub mod accumulator;
 pub mod demuxer;
 pub mod muxer;
-pub mod playlist;
-pub mod streamkey;
-
-#[derive(Debug, Clone)]
-pub struct AccessUnit {
-    pub key: bool,
-    pub pts: u64,
-    pub dts: u64,
-    pub data: Bytes,
-    pub avc: bool,
-}
